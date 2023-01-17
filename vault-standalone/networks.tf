1f9fdd29a3789235bb7284a06ecc46d5c4945cff
conor-mccullough <103977699+conor-mccullough@users.noreply.github.com>
2023-01-17 14:05:18 +1100

    Update networks.tf

--- vault-standalone/networks.tf
+++ vault-standalone/networks.tf
@@ -1,3 +1,4 @@
+# Create and populate the rules of a security group to allow access to/from the server
 resource "aws_security_group" "conor-vault-sg" {
 
   name = "conor-vault-sg"
@@ -35,20 +36,21 @@ resource "aws_security_group" "conor-vault-sg" {
   }
 }
 
+# Create a VPC for the Vault server to use
 resource "aws_vpc" "conor-vault-vpc" {
   cidr_block       = "10.0.0.0/16"
-#  security_groups = ["${aws_security_group.conor-vault-sg.id}"]
   tags = {
     Name = "Conor Vault VPC"
   }
 }
 
+# Make sure the route table we create, "conor-route-table", is associated with our VPC
 resource "aws_main_route_table_association" "conor-vpc-route-association" {
   vpc_id = aws_vpc.conor-vault-vpc.id
   route_table_id = aws_route_table.conor-route-table.id
 }
 
-# Created this because vpc_id is not supported for aws_instance resource type:
+# Created this because vpc_id is not supported for aws_instance resource type
 resource "aws_subnet" "conor-subnet" {
   vpc_id            = aws_vpc.conor-vault-vpc.id
   cidr_block        = "10.0.0.0/24"
@@ -59,15 +61,12 @@ resource "aws_subnet" "conor-subnet" {
   }
 }
 
-
-# I was unable to connect to the instance, even with a public IP, so adding an IGW:
+# Add an Internet Gateway to the VPC so it's able to reach the internet, provide our new internet gateway ID for each of the public route's gateways
 resource "aws_internet_gateway" "conor-gw" {
   vpc_id = aws_vpc.conor-vault-vpc.id
 }
 
-# The route table also needed the IGW attached to it:
-# Current issue is it's creating a second route table for the VPC, which the instance is attaching to, and the route table with the CORRECT info is not being attached correctly.
-# Consider https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#main_route_table_id  -  for establishing this connection
+# Create the route table, attach it to the VPC we created
 resource "aws_route_table" "conor-route-table" {
   vpc_id = aws_vpc.conor-vault-vpc.id
 
@@ -81,24 +80,7 @@ resource "aws_route_table" "conor-route-table" {
     gateway_id = aws_internet_gateway.conor-gw.id
   }
 
-#  route {
-#    ipv6_cidr_block        = "::/0"
-#    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
-#  }
-
   tags = {
     Name = "Route table for TF tests"
   }
 }
-
-### Check the route table. It needs to have both the local 10.0.0.0/16 route and the 0.0.0.0/0 dst route (target should be the igw being set up)
-
-/*
-The aws_route_table resource you posted creates a new route table in your VPC. It is possible that you have another route table in your VPC that was created outside of this configuration or by another configuration.
-
-By default, when you create a VPC, it automatically creates a main route table and assigns it to the VPC. This main route table has a default route (0.0.0.0/0) that sends all Internet-bound traffic to an Internet Gateway (if you have created one in your VPC).
-
-The aws_route_table resource in your configuration creates an additional route table in your VPC, and it has a name of "Route table for TF tests". This route table has both a private route (10.0.0.0/16) and a public route (0.0.0.0/0) that allows all outgoing traffic from the VPC to be routed out to the internet via the Internet Gateway.
-
-Therefore, you should see two route tables in your VPC: one main route table and one additional route table that was created by your configuration.
-*/
